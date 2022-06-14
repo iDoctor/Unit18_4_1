8339aacf124b9688eeee14d864f9bee48977f239
Roman Filatov <ikardoc@gmail.com>
2022-06-14 21:02:23 +0300

    Added Logic

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+Console.WriteLine("Введите ссылку на видео с Youtube:");
+string link = Console.ReadLine();
+
+var youtube = new YoutubeClient();
+var video = await youtube.Videos.GetAsync(link);
+
+while (true)
+{
+    Console.WriteLine($"{Environment.NewLine}Введите команду:");
+    Console.WriteLine("Информация о видео = i");
+    Console.WriteLine("Скачивание видео = d");
+    string command = Console.ReadLine();
+
+    if (command == "i")
+    {
+        var videoInfo = $"{video.Author.ChannelTitle} | {video.Title} | {video.Duration}";
+        Console.WriteLine($"{Environment.NewLine}Информация о видео:{Environment.NewLine}{videoInfo}");
+    }
+    else if (command == "d")
+    {
+        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
+        var streamInfo = streamManifest
+            .GetVideoOnlyStreams()
+            .Where(s => s.Container == Container.Mp4)
+            .GetWithHighestVideoQuality();
+
+        await youtube.Videos.Streams.DownloadAsync(streamInfo, $"video.{streamInfo.Container}");
+
+        Console.WriteLine($"{Environment.NewLine}Видео скачено!");
+    }
+}
